436727df2fe86dca30b995dc85b33518a9c86a9a
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-09 20:32:01 -0600

    Created a player class and imported it into adv.py.

--- src/player.py
+++ src/player.py
@@ -1,2 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, title):
+        self.name = name
+        self.title = title
+
+    def __str__(self):
+        output = f"Hello! My name is, {self.name}. I am a {self.title}."
+        return output
+
+
+    def __repr__(self):
+        # also returns a string which helps devs understand how your object is structured.
+        return f"self.name = {self.name} ; self.title = {self.title}"
+
+player1 = Player("Tyler", "Sorcerer")
+
+print(player1)
