051d7e5fa2fbfb63c19483ce0b74387412be7360
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-11 09:53:42 -0600

    Added cardinal directions to room.py as well as text outputs for each.

--- src/room.py
+++ src/room.py
@@ -5,9 +5,23 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.e_to = None
+        self.s_to = None
+        self.w_to = None
+
 
     def __str__(self):
         output = f"Welcome to the {self.name}. {self.description}"
+        if self.n_to:
+            output += f'To the North is: {self.n_to.name} \n'
+        if self.e_to:
+            output += f'To the North is: {self.e_to.name} \n'
+        if self.s_to:
+            output += f'To the North is: {self.s_to.name} \n'
+        if self.w_to:
+            output += f'To the North is: {self.w_to.name} \n'
+            
         return output
 
 
