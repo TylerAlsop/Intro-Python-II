28d0cff82b829765fb3637d4c13c73bb8183a127
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-11 20:34:57 -0600

    Changed items to inventory in player.py.

--- src/player.py
+++ src/player.py
@@ -1,9 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, current_room = None, items):
+    def __init__(self, current_room = None, inventory):
         self.current_room = current_room
-        self.items = items
+        self.inventory = inventory
 
     # def __str__(self):
     #     output = f"You am currently in the {self.current_room}."
